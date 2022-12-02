d8a47c708ed6d9845b3a9165ed156566e92e547c
Henrique Andrade <henrique.ccda@gmail.com>
2022-12-02 19:44:54 -0300

    m

---
+++ 04processos/pool_python_v2.py
@@ -0,0 +1,30 @@
+import multiprocessing
+
+
+def calcular(dado):
+    return dado ** 2
+
+
+def imprimir_nome_processo():
+    print(f'Iiciando o processo com nome: {multiprocessing.current_process().name}')
+
+
+def main():
+    tamanaho_pool = multiprocessing.cpu_count() * 2
+
+    print(f'Tamanho da Pool: {tamanaho_pool}')
+
+    pool = multiprocessing.Pool(processes=tamanaho_pool, initializer=imprimir_nome_processo)
+
+    entradas = list(range(7))
+
+    saidas = pool.map(calcular, entradas)
+
+    print(f'Saídas: {saidas}')
+
+    pool.close()
+    pool.join()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 04processos/processo.py
@@ -0,0 +1,19 @@
+import multiprocessing
+
+print(f'Inicioando o processo com nome: {multiprocessing.current_process().name}')
+
+def faz_algo(valor):
+    print(f'Fazendo algo com o {valor}')
+
+def main():
+
+    pc = multiprocessing.Process(target=faz_algo, args=('Passaro', ), name='Processo Geek')
+
+    print(f'Iniciando o processo com nome: {pc.name}')
+
+    pc.start()
+    pc.join()
+
+
+if __name__ == '__main__':
+    main()
