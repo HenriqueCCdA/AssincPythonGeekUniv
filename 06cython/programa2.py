d52bdb1e744f7449d30739e9f6408e01117cb4d4
Henrique Andrade <henrique.ccda@gmail.com>
2022-12-11 12:46:50 -0300

    m

---
+++ 06cython/programa2.py
@@ -0,0 +1,19 @@
+import datetime
+import computa
+
+def main():
+    inicio = datetime.datetime.now()
+
+    computa.computar(fim=50_000_000)
+
+    tempo = datetime.datetime.now() - inicio
+
+    print(f'Terminou em {tempo.total_seconds():.2f} segundos.')
+
+
+if __name__ == '__main__':
+    main()
+
+'''
+Terminou em 20.79 s
+'''
